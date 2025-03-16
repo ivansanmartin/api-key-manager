6d206884f67b0fc27c98e9c4993f1d4fab7406c4
ivansanmartin <ivansanmartin987@gmail.com>
2025-03-16 12:19:44 -0300

    test cors

--- app/main.py
+++ app/main.py
@@ -2,8 +2,23 @@ from fastapi import FastAPI, Request, status
 from app.api.v1.endpoints.api_key_manager import router as api_key_router
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+origins = [
+    "https://ivsm.link",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(api_key_router, prefix="/api/v1")
 
